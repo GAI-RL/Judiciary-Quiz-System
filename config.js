3e30346b29db4d0d0a9a64bcf0e1f4991fa31421
GAI-RL <airl@kics.edu.pk>
2025-10-27 05:13:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const SUPABASE_CONFIG = {
 };
 
 const QUIZ_SETTINGS = {
-  TOTAL_TIME: 120,   // total time in seconds (5 minutes)
+  TOTAL_TIME: 600,   // total time in seconds (5 minutes)
   TOTAL_QUESTIONS: 30,
   SHOW_PROGRESS_BAR: true,
   RANDOMIZE_QUESTIONS: true,
@@ -18,3 +18,4 @@ let score = 0;
 let timer = QUIZ_SETTINGS.TOTAL_TIME;
 let timerInterval;
 let selectedOption = null;
+
