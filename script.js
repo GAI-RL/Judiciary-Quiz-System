029ac847aa12983e70049293c0fb37616d581f2b
GAI-RL <airl@kics.edu.pk>
2025-10-29 01:18:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -321,7 +321,7 @@ nextBtn.addEventListener("click", () => {
 
   // Score only if answer exists
   if (q.options && q.answer && selectedOption === q.answer) {
-    score++;
+    score *2;
     console.log("Score is " + score);
   }
 
@@ -522,5 +522,6 @@ if (fileAnswer) {
 //     }
 //   });
 // });
+
 
 
